a6d938929b6eb77be1f12267df1883cddc9aa78d
Andre Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-08-02 00:25:42 -0300

    feat: create ErrosMiddlewares

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,18 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import ErrorsMiddlewares from '@shared/middlewares/ErrorsMiddlewares';
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(routes);
+
+app.use(ErrorsMiddlewares);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! ☁️');
