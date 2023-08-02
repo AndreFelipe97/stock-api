8a954492ae9ee9c5a30fbbbec82d91e8da38ebab
Andre Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-08-02 19:54:16 -0300

    chore: add typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
-import express, { NextFunction, Request, Response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-import AppError from '@shared/errors/AppError';
 import ErrorsMiddlewares from '@shared/middlewares/ErrorsMiddlewares';
+import '@shared/typeorm';
 
 const app = express();
 
