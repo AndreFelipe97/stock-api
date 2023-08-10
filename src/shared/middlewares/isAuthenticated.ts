65910f725154540adf5a99cc04b02a15c73cd703
Andre Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-08-09 23:29:27 -0300

    feat: create middleware isAuthenticated

--- src/shared/middlewares/isAuthenticated.ts
+++ src/shared/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -16,6 +22,12 @@ export default function isAuthenticated(
 
   try {
     const decodeToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodeToken as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch (e) {
